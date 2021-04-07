72b28a8b71401055d261d7a4c26b83c0cad2b45f
chentimothy1 <timothychen1@protonmail.com>
2021-04-06 17:21:14 -0700

    song search via genre then playlist

--- src/Credentials.js
+++ src/Credentials.js
@@ -1,5 +1,6 @@
 const Credentials = () => {
 
+    //change to your id/secret
     return {
         ClientId: '1b2680cc967d45ca999452fbcad4f25d',
         ClientSecret: '925d23c600ac4a10bff1c0604d0b43a0'
