b831425d404f99f18944f61c25b458a866e55057
chentimothy1 <timothychen1@protonmail.com>
2021-04-07 12:40:12 -0700

    testing

---
+++ src/Credentials.js
@@ -0,0 +1,10 @@
+const Credentials = () => {
+
+    //change to your id and secret
+    return {
+        ClientId: '1b2680cc967d45ca999452fbcad4f25d',
+        ClientSecret: '925d23c600ac4a10bff1c0604d0b43a0'
+    }
+}
+
+export { Credentials };
