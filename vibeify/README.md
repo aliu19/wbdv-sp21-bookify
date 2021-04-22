c31ceb7921381dbeeb70039ccbb4fff60f2a3438
chentimothy1 <timothychen1@protonmail.com>
2021-04-21 21:10:45 -0700

    started on homepage UI

--- vibeify/README.md
+++ vibeify/README.md
@@ -1,8 +1,3 @@
-steps:
-clone project
-npm install
-npm start
-
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ vibeify/src/components/Nav.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import logo from '../spotify-2.svg'
+
+
+const Nav = () => {
+    return (
+        <div className="navBar">
+        <div className="logo">
+          <img src={logo} width="100" height="50"/>
+          Vibeify
+        </div>
+        <ul>
+          <li>Home</li>
+          <li>Search</li>
+          <li>Library</li>
+        </ul>
+        <div className="cookies">
+            <span>Cookies</span>
+            <span>Privacy Policy</span>
+
+        </div>
+      </div>
+    )
+
+}
+
+export default Nav
