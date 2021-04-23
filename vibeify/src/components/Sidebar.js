a7f03d578eea549662144869067279ddaaa72f00
chentimothy1 <timothychen1@protonmail.com>
2021-04-22 17:48:06 -0700

    updated sidebar to have icons

--- vibeify/src/components/Sidebar.js
+++ vibeify/src/components/Sidebar.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import './Sidebar.css';
-import { ReactComponent as SpotifyLogo } from '../svgs/spotify-logo.svg'
+import SidebarOption from './SidebarOption';
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
+
 
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <SpotifyLogo />
+            <img
+            className="sidebar_logo"
+                src="https://developer.spotify.com/assets/branding-guidelines/icon3@2x.png"
+                alt=""
+            />
+            <SidebarOption Icon={HomeIcon} title="Home"/>
+            <SidebarOption Icon={SearchIcon} title="Search"/>
+            <SidebarOption Icon={LibraryMusicIcon} title="Your Library"/>
+
             </div>
 
     )
