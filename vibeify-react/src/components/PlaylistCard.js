f1c2991239d395ae6ac5ef0afd241984fbbcb108
Srilekha Nuli <srilekhanuli@gmail.com>
2021-04-25 23:47:13 -0400

    add link to playlist card

--- vibeify-react/src/components/PlaylistCard.js
+++ vibeify-react/src/components/PlaylistCard.js
@@ -1,11 +1,12 @@
 import { useDataLayerValue } from './DataLayer';
 import './components_css/PlaylistCard.css'
+import { Link } from 'react-router-dom'
 
 const PlaylistCard = ({ playlist }) => {
-    return playlist ? (<div className="playlist-card">
+    return playlist ? (<Link className="playlist-card">
         <img className="playlist-card__image" src={playlist.images[0].url}></img>
         <p className="playlist-card__name">{playlist.name}</p>
-    </div>) : null
+    </Link>) : null
 }
 
 export default PlaylistCard
