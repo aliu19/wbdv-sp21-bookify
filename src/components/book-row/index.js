305230117a5dd327ce8db51ff8ed2bb2c66bb011
Srilekha Nuli <srilekhanuli@gmail.com>
2021-04-29 10:08:35 -0400

    fix responsiveness of book row

--- src/components/book-row/index.js
+++ src/components/book-row/index.js
@@ -5,8 +5,8 @@ const BookRow = ({ book }) => {
     return (
         <Link className="d-flex" to={`/details/${book.id}`}>
             <div className="row w-100">
-                <div className="col-11">{book.volumeInfo.title}</div>
-                <div className="col-1">
+                <div className="col-12 col-md-11">{book.volumeInfo.title}</div>
+                <div className="d-none d-md-block col-1">
                     {book.volumeInfo.imageLinks.smallThumbnail ?
                         <img className="img-fluid"
                             src={book.volumeInfo.imageLinks.smallThumbnail}
