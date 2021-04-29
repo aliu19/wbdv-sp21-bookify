b00ead7e5730f19cb186f695cd278659b5887a18
aliu19 <liu.and@northeastern.edu>
2021-04-29 04:27:50 -0400

    details page setup

--- src/components/search/details.js
+++ src/components/search/details.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {useParams} from 'react-router-dom'
+import bookService from '../../services/book-service'
 
 const Details = () => {
+
+  const {bookId} = useParams()
+  const [book, setBook] = useState({})
+  const [cover, setCover] = useState("")
+  // const [authors, setAuthors] = useState({})
+
+  useEffect(() => {
+    bookService.findBookById(bookId)
+    .then(result => {
+      setBook(result.volumeInfo)
+      setCover(result.volumeInfo.imageLinks)
+      // setAuthors(result.volumeInfo.authors)
+    })
+  }, [])
+
   return(
       <div>
-        <h1>Details</h1>
-        <img src=""/>
-        <h6>
-          Album Name
-        </h6>
-        <ul>
-          <li>Artist 1</li>
-          <li>Artist 2</li>
-        </ul>
+        <img src={cover.thumbnail}/>
+        <h1>{book.title}</h1>
+        <h3>By: {book.authors}</h3>
+        <h3>Synopsis:</h3>
+        <p>
+          {book.description}
+        </p>
       </div>
   )
 }
