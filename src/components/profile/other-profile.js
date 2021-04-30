0552787ad844d103760c71a25d98ac78ca60dbaa
Srilekha Nuli <srilekhanuli@gmail.com>
2021-04-30 13:42:35 -0400

    idk

--- src/components/profile/other-profile.js
+++ src/components/profile/other-profile.js
@@ -1,13 +1,13 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import userService from "../../services/user-service";
-import {useParams} from "react-router";
+import { useParams } from "react-router";
 import reviewService from '../../services/review-service';
 import ReviewList from '../reviews/review-list';
 // import BookList from '../book-list';
 
 const OtherProfile = () => {
 
-  const {profileId} = useParams()
+  const { profileId } = useParams()
   const [currentUser, setCurrentUser] = useState({})
   const [otherUser, setOtherUser] = useState({})
   const [reviews, setReviews] = useState({})
@@ -15,16 +15,16 @@ const OtherProfile = () => {
 
   useEffect(() => {
     userService.profile()
-    .then((currentUser) => {
-      setCurrentUser(currentUser)
-    })
+      .then((currentUser) => {
+        setCurrentUser(currentUser)
+      })
   }, [])
 
   useEffect(() => {
     userService.getUserById(profileId)
-    .then((user) => {
-      setOtherUser(user)
-    })
+      .then((user) => {
+        setOtherUser(user)
+      })
   }, [profileId])
 
   useEffect(() => {
@@ -37,91 +37,92 @@ const OtherProfile = () => {
 
   const updateUser = () => {
     userService.updateUser(otherUser._id, otherUser)
-    .then((updatedUser) => {
-      setOtherUser(updatedUser)
-    })
+      .then((updatedUser) => {
+        setOtherUser(updatedUser)
+      })
   }
 
   return (
-      <div>
-        <h1>
-          {otherUser.username}
-        </h1>
-        {
-          currentUser.role === "ADMIN" &&
-          <div>
-            <form className="container-fluid">
-              <div className="row mb-2">
-                <label>Username</label>
-                <input className="form-control"
-                       onChange={(e) => {setOtherUser({...otherUser, username: e.target.value})}}
-                       value={otherUser.username}></input>
-              </div>
-              <div className="row mb-2">
-                <label>Password</label>
-                <input className="form-control"
-                       type="password"
-                       onChange={(e) => {setOtherUser({...otherUser, password: e.target.value})}}
-                       value={otherUser.password}></input>
-              </div>
-              <div className="row mb-2">
-                <label>First name</label>
-                <input className="form-control"
-                       onChange={(e) => {setOtherUser({...otherUser, firstName: e.target.value})}}
-                       value={otherUser.firstName}></input>
-              </div>
-              <div className="row mb-2">
-                <label>Last name</label>
-                <input className="form-control"
-                       onChange={(e) => {setOtherUser({...otherUser, lastName: e.target.value})}}
-                       value={otherUser.lastName}></input>
-              </div>
-              <div className="row mb-2">
-                <label>Email</label>
-                <input className="form-control"
-                       type="email"
-                       onChange={(e) => {setOtherUser({...otherUser, email: e.target.value})}}
-                       value={otherUser.email}></input>
-              </div>
-              <div className="row mb-4">
-                <label>Role</label>
-                <select className="form-control"
-                        onChange={(e) => { setOtherUser({ ...otherUser, role: e.target.value }) }}
-                        value={otherUser.role}
-                        disabled={otherUser.role === "ADMIN" && "disabled"}>
-                  <option value="none" className="selected disabled hidden"/>
-                  <option value="GENERAL_USER">General User</option>
-                  <option value="ADMIN">Admin</option>
-                </select>
-              </div>
-              <div className="row mb-4 justify-content-between">
-                <button className="btn btn-primary"
-                        onClick={updateUser}>
-                  Update
+    <div>
+      <h1>
+        {otherUser.username}
+      </h1>
+      {
+        currentUser.role === "ADMIN" &&
+        <div>
+          <form className="container-fluid">
+            <div className="row mb-2">
+              <label>Username</label>
+              <input className="form-control"
+                onChange={(e) => { setOtherUser({ ...otherUser, username: e.target.value }) }}
+                value={otherUser.username}></input>
+            </div>
+            <div className="row mb-2">
+              <label>Password</label>
+              <input className="form-control"
+                type="password"
+                onChange={(e) => { setOtherUser({ ...otherUser, password: e.target.value }) }}
+                value={otherUser.password}></input>
+            </div>
+            <div className="row mb-2">
+              <label>First name</label>
+              <input className="form-control"
+                onChange={(e) => { setOtherUser({ ...otherUser, firstName: e.target.value }) }}
+                value={otherUser.firstName}></input>
+            </div>
+            <div className="row mb-2">
+              <label>Last name</label>
+              <input className="form-control"
+                onChange={(e) => { setOtherUser({ ...otherUser, lastName: e.target.value }) }}
+                value={otherUser.lastName}></input>
+            </div>
+            <div className="row mb-2">
+              <label>Email</label>
+              <input className="form-control"
+                type="email"
+                onChange={(e) => { setOtherUser({ ...otherUser, email: e.target.value }) }}
+                value={otherUser.email}></input>
+            </div>
+            <div className="row mb-4">
+              <label>Role</label>
+              <select className="form-control"
+                onChange={(e) => { setOtherUser({ ...otherUser, role: e.target.value }) }}
+                value={otherUser.role}
+                disabled={otherUser.role === "ADMIN" && "disabled"}>
+                <option value="none" className="selected disabled hidden" />
+                <option value="GENERAL_USER">General User</option>
+                <option value="ADMIN">Admin</option>
+              </select>
+            </div>
+            <div className="row mb-4 justify-content-between">
+              <button className="btn btn-primary"
+                onClick={updateUser}>
+                Update
                 </button>
-              </div>
-            </form>
-          </div>
-        }
-        {
-          otherUser && otherUser.username &&
-          (<div>
-            <h2 className="h3">
-              Reviews by {otherUser && otherUser.username}
-            </h2>
-            <ReviewList reviews={reviews} />
-            <h2 className="h3">
-              Booklists by {otherUser && otherUser.username}
-            </h2>
-            {/*{!!bookLists.length && bookLists.map(b => <BookList bookList={b} />)}*/}
-          </div>)
-        }
-           
+            </div>
+          </form>
+        </div>
+      }
+      {
+        otherUser && otherUser.username &&
+        (<div>
+          <h2 className="h3">
+            Reviews by {otherUser && otherUser.username}
+          </h2>
+          <ReviewList reviews={reviews} />
+          <h2 className="h3">
+            Booklists by {otherUser && otherUser.username}
+          </h2>
+          {/*{!!bookLists.length && bookLists.map(b => <BookList bookList={b} />)}*/}
+        </div>)
+      }
+
       {/* {JSON.stringify(user)} */}
       {/* {JSON.stringify(bookLists)} */}
       {/* {JSON.stringify(reviews)} */}
       {/*  TODO GetUser*/}
-      </div>
+    </div>
   )
 }
+
 export default OtherProfile
--- src/components/profile/your-profile.js
+++ src/components/profile/your-profile.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import userService from "../../services/user-service"
 
 const YourProfile = () => {
@@ -6,79 +6,80 @@ const YourProfile = () => {
   const [currentUser, setCurrentUser] = useState({})
   const updateUser = () => {
     userService.updateUser(currentUser._id, currentUser)
-    .then((updatedUser) => {
-      setCurrentUser(updatedUser)
-    })
+      .then((updatedUser) => {
+        setCurrentUser(updatedUser)
+      })
   }
 
   useEffect(() => {
     userService.profile()
-    .then((currentUser) => {
-      setCurrentUser(currentUser)
-    })
+      .then((currentUser) => {
+        setCurrentUser(currentUser)
+      })
   }, [])
 
   return (
-      <div>
-        <h1>
-          Welcome {currentUser.username}
-        </h1>
+    <div>
+      <h1>
+        Welcome {currentUser.username}
+      </h1>
 
-        <form className="container-fluid">
-          <div className="row mb-2">
-            <label>Username</label>
-            <input className="form-control"
-                   onChange={(e) => {setCurrentUser({...currentUser, username: e.target.value})}}
-                   value={currentUser.username}></input>
-          </div>
-          <div className="row mb-2">
-            <label>Password</label>
-            <input className="form-control"
-                   onChange={(e) => {setCurrentUser({...currentUser, password: e.target.value})}}
-                   value={currentUser.password}></input>
-          </div>
-          <div className="row mb-2">
-            <label>First name</label>
-            <input className="form-control"
-                   onChange={(e) => {setCurrentUser({...currentUser, firstName: e.target.value})}}
-                   value={currentUser.firstName}></input>
-          </div>
-          <div className="row mb-2">
-            <label>Last name</label>
-            <input className="form-control"
-                   onChange={(e) => {setCurrentUser({...currentUser, lastName: e.target.value})}}
-                   value={currentUser.lastName}></input>
-          </div>
-          <div className="row mb-2">
-            <label>Email</label>
-            <input className="form-control"
-                   type="email"
-                   onChange={(e) => {setCurrentUser({...currentUser, email: e.target.value})}}
-                   value={currentUser.email}></input>
-          </div>
-          <div className="row mb-4">
+      <form className="container-fluid">
+        <div className="row mb-2">
+          <label>Username</label>
+          <input className="form-control"
+            onChange={(e) => { setCurrentUser({ ...currentUser, username: e.target.value }) }}
+            value={currentUser.username}></input>
+        </div>
+        <div className="row mb-2">
+          <label>Password</label>
+          <input className="form-control"
+            onChange={(e) => { setCurrentUser({ ...currentUser, password: e.target.value }) }}
+            value={currentUser.password}></input>
+        </div>
+        <div className="row mb-2">
+          <label>First name</label>
+          <input className="form-control"
+            onChange={(e) => { setCurrentUser({ ...currentUser, firstName: e.target.value }) }}
+            value={currentUser.firstName}></input>
+        </div>
+        <div className="row mb-2">
+          <label>Last name</label>
+          <input className="form-control"
+            onChange={(e) => { setCurrentUser({ ...currentUser, lastName: e.target.value }) }}
+            value={currentUser.lastName}></input>
+        </div>
+        <div className="row mb-2">
+          <label>Email</label>
+          <input className="form-control"
+            type="email"
+            onChange={(e) => { setCurrentUser({ ...currentUser, email: e.target.value }) }}
+            value={currentUser.email}></input>
+        </div>
+        <div className="row mb-4">
           <label>Role</label>
           <select className="form-control"
             onChange={(e) => { setCurrentUser({ ...currentUser, role: e.target.value }) }}
             value={currentUser.role}
             disabled="disabled">
+            {/*<option value="none" className="selected disabled hidden" />*/}
             <option value="GENERAL_USER">General User</option>
             <option value="ADMIN">Admin</option>
           </select>
-          </div>
-          <div className="row mb-4 justify-content-between">
-            <button className="btn btn-primary"
-                    onClick={updateUser}>
-              Update
+        </div>
+        <div className="row mb-4 justify-content-between">
+          <button className="btn btn-primary"
+            onClick={updateUser}>
+            Update
             </button>
-          </div>
-        </form>
+        </div>
+      </form>
 
       {/*
       TODO UpdateUser, DeleteUser
       TODO show username, password, first name, last name, email, role
       TODO render book lists and reviews the user made*/}
-      </div>
+    </div>
   )
 }
 export default YourProfile
